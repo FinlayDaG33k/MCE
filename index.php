520bcc656e224e6dcd46ed9504d2cb96abb709b4
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-03-08 11:37:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ if (!empty($_FILES)){
 ?>
 <form enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
     .mcz file: <input name="mcz" type="file" /><br />
+		Money: <input type="text" name="money" value="100000"><br>
 		<input type="checkbox" name="finishbuildings" value="finishbuildings"> Finish all my buildings<br>
     <input type="submit" value="Send File" />
 </form>
