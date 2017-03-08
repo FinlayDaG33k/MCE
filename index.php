1a093d8609ccfe68840ab5e60ca93bef322e4aa5
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-03-08 15:39:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,90 @@ function finishBuildings($data){
 function setMoney($data,$total){
 	$data['resources']['Money']['amount'] = intval($total);
 	$data['resources']['Money']['previous'] = intval($total);
-	$total_goldmint = intval ($total + $data['resources']['Money']['inflows']['total']['Gold Mint']);
-	$data['resources']['Money']['inflows']['total']['Gold Mint'] = intval ($total_goldmint);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Gold Mint']);
+	$data['resources']['Money']['inflows']['total']['Gold Mint'] = intval ($total_earned);
 	$data['resources']['Money']['inflows']['allCurrent'] = intval($data['resources']['Money']['inflows']['allCurrent'] + $total);
 	$data['resources']['Money']['inflows']['allTime'] = intval ($data['resources']['Money']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setOre($data,$total){
+	$data['resources']['Ore']['amount'] = intval($total);
+	$data['resources']['Ore']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Ore']['inflows']['total']['Harvesting']);
+	$data['resources']['Ore']['inflows']['total']['Harvesting'] = intval ($total_earned);
+	$data['resources']['Ore']['inflows']['allCurrent'] = intval($data['resources']['Ore']['inflows']['allCurrent'] + $total);
+	$data['resources']['Ore']['inflows']['allTime'] = intval ($data['resources']['Ore']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setGold($data,$total){
+	$data['resources']['Gold']['amount'] = intval($total);
+	$data['resources']['Gold']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Harvesting']);
+	$data['resources']['Gold']['inflows']['total']['Harvesting'] = intval ($total_earned);
+	$data['resources']['Gold']['inflows']['allCurrent'] = intval($data['resources']['Gold']['inflows']['allCurrent'] + $total);
+	$data['resources']['Gold']['inflows']['allTime'] = intval ($data['resources']['Gold']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setCivics($data,$total){
+	$data['resources']['Civics']['amount'] = intval($total);
+	$data['resources']['Civics']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Civics']['inflows']['total']['Civic Center']);
+	$data['resources']['Civics']['inflows']['total']['Civic Center'] = intval ($total_earned);
+	$data['resources']['Civics']['inflows']['allCurrent'] = intval($data['resources']['Civics']['inflows']['allCurrent'] + $total);
+	$data['resources']['Civics']['inflows']['allTime'] = intval ($data['resources']['Civics']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setMicrochip($data,$total){
+	$data['resources']['Microchip']['amount'] = intval($total);
+	$data['resources']['Microchip']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Microchip Factory']);
+	$data['resources']['Microchip']['inflows']['total']['Microchip Factory'] = intval ($total_earned);
+	$data['resources']['Microchip']['inflows']['allCurrent'] = intval($data['resources']['Microchip']['inflows']['allCurrent'] + $total);
+	$data['resources']['Microchip']['inflows']['allTime'] = intval ($data['resources']['Microchip']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setAtmosphere($data,$total){
+	$data['resources']['Atmosphere']['amount'] = intval($total);
+	$data['resources']['Atmosphere']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Ore Refinery']);
+	$data['resources']['Atmosphere']['inflows']['total']['Ore Refinery'] = intval ($total_earned);
+	$data['resources']['Atmosphere']['inflows']['allCurrent'] = intval($data['resources']['Atmosphere']['inflows']['allCurrent'] + $total);
+	$data['resources']['Atmosphere']['inflows']['allTime'] = intval ($data['resources']['Atmosphere']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setResearch($data,$total){
+	$data['resources']['Research']['amount'] = intval($total);
+	$data['resources']['Research']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Small Research Lab']);
+	$data['resources']['Research']['inflows']['total']['Small Research Lab'] = intval ($total_earned);
+	$data['resources']['Research']['inflows']['allCurrent'] = intval($data['resources']['Research']['inflows']['allCurrent'] + $total);
+	$data['resources']['Research']['inflows']['allTime'] = intval ($data['resources']['Research']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setAluminum($data,$total){
+	$data['resources']['Aluminum']['amount'] = intval($total);
+	$data['resources']['Aluminum']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Harvesting']);
+	$data['resources']['Aluminum']['inflows']['total']['Harvesting'] = intval ($total_earned);
+	$data['resources']['Aluminum']['inflows']['allCurrent'] = intval($data['resources']['Aluminum']['inflows']['allCurrent'] + $total);
+	$data['resources']['Aluminum']['inflows']['allTime'] = intval ($data['resources']['Aluminum']['inflows']['allTime'] + $total);
+	return $data;
+}
+
+function setUranium($data,$total){
+	$data['resources']['Uranium']['amount'] = intval($total);
+	$data['resources']['Uranium']['previous'] = intval($total);
+	$total_earned = intval ($total + $data['resources']['Money']['inflows']['total']['Harvesting']);
+	$data['resources']['Uranium']['inflows']['total']['Harvesting'] = intval ($total_earned);
+	$data['resources']['Uranium']['inflows']['allCurrent'] = intval($data['resources']['Uranium']['inflows']['allCurrent'] + $total);
+	$data['resources']['Uranium']['inflows']['allTime'] = intval ($data['resources']['Uranium']['inflows']['allTime'] + $total);
 	return $data;
 }
 
@@ -31,14 +111,27 @@ if (!empty($_FILES)){
 	}
 
 	$mcz = setMoney($mcz,$_POST['money']);
-	
+	$mcz = setOre($mcz,$_POST['ore']);
+	$mcz = setGold($mcz,$_POST['gold']);
+	$mcz = setCivics($mcz,$_POST['civics']);
+	$mcz = setMicrochip($mcz,$_POST['microchip']);
+	$mcz = setAtmosphere($mcz,$_POST['atmosphere']);
+	$mcz = setAluminum($mcz,$_POST['aluminum']);
+	$mcz = setUranium($mcz,$_POST['uranium']);
 	echo base64_encode(json_encode($mcz));
 }else{
 
 ?>
 <form enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
     .mcz file: <input name="mcz" type="file" /><br />
-		Money: <input type="text" name="money"><br>
+		Money: <input type="text" name="money" value="100000"><br>
+		Ore: <input type="text" name="ore" value="100000"><br>
+		Gold: <input type="text" name="gold" value="100000"><br>
+		Civics: <input type="text" name="civics" value="100000"><br>
+		Microchip: <input type="text" name="microchip" value="100000"><br>
+		Atmosphere: <input type="text" name="atmosphere" value="100000"><br>
+		Aluminum: <input type="text" name="aluminum" value="100000"><br>
+		Uranium: <input type="text" name="uranium" value="100000"><br>
 		<input type="checkbox" name="finishbuildings" value="finishbuildings"> Finish all my buildings<br>
     <input type="submit" value="Send File" />
 </form>
