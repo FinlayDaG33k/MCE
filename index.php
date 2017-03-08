3138df8da9902c0fc7c5b41c35368bbe11ad10ff
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2017-03-08 11:41:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,14 +25,20 @@ function setMoney($data,$total){
 
 if (!empty($_FILES)){
 	$mcz = json_decode(base64_decode(file_get_contents($_FILES['mcz']['tmp_name'])),1);
-	echo json_encode(setMoney($mcz,$_POST['money']));
-	//echo base64_encode(json_encode(finishBuildings($mcz)));
+	//echo json_encode(setMoney($mcz,$_POST['money']));
+	if($_POST['finishbuildings']){
+		$mcz = finishBuildings($mcz);
+	}
+	if($_POST['money'] > $mcz['resources']['Money']['amount']){
+		$mcz = setMoney($mcz,$_POST['money']);
+	}
+	echo base64_encode(json_encode($mcz));
 }else{
 
 ?>
 <form enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
     .mcz file: <input name="mcz" type="file" /><br />
-		Money: <input type="text" name="money" value="100000"><br>
+		Money: <input type="text" name="money"><br>
 		<input type="checkbox" name="finishbuildings" value="finishbuildings"> Finish all my buildings<br>
     <input type="submit" value="Send File" />
 </form>
